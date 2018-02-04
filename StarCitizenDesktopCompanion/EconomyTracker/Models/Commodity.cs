d14c4e2e5a5b5bcba86a1c067184fd96e57fda49
Gregory Hanson <gregorydhanson117@gmail.com>
2018-02-04 10:35:31 -0500

    Added models and basic gui segregation

---
+++ StarCitizenDesktopCompanion/EconomyTracker/Models/Commodity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomyTracker.Models
+{
+    public class Commodity
+    {
+        public Guid Guid { get; }
+        public string Name { get; }
+    }
+}
---
+++ StarCitizenDesktopCompanion/EconomyTracker/Models/Interfaces/ILocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomyTracker.Models.Interfaces
+{
+    public interface ILocation
+    {
+        Guid Guid { get; }
+        string Name { get; }
+        List<Location> Children { get; }
+    }
+}
