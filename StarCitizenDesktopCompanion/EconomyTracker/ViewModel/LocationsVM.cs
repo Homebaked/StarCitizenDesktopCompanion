550640fc071e1f0480ac03919544a233bf0dac3e
Gregory Hanson <gregorydhanson117@gmail.com>
2018-02-26 21:53:22 -0500

    Added LocationsView

---
+++ StarCitizenDesktopCompanion/EconomyTracker/ViewModel/LocationsVM.cs
@@ -0,0 +1,16 @@
+﻿using EconomyTracker.Models;
+using EconomyTracker.Models.Interfaces;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomyTracker.ViewModel
+{
+    public class LocationsVM : ViewModelBase
+    {
+        public LocationsVM(IEnumerable<ILocation> )
+    }
+}
