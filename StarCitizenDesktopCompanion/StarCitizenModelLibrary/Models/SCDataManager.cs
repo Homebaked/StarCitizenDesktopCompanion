e1d6106d84e9022b84dff50778c1bf5eab4136b6
Gregory Hanson <gregorydhanson117@gmail.com>
2018-07-13 17:26:31 -0400

    Finished DataManager model

--- StarCitizenDesktopCompanion/StarCitizenModelLibrary/Models/SCDataManager.cs
+++ StarCitizenDesktopCompanion/StarCitizenModelLibrary/Models/SCDataManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace StarCitizenModelLibrary.Models
 {
     public class SCDataManager : BaseModel
     {
+        public ObservableCollection<Commodity> Commodities { get; }
+        public ObservableCollection<TradingPort> TradingPorts { get; }
 
+        public SCDataManager(Guid guid = new Guid()) : base(guid)
+        {
+            this.Commodities = new ObservableCollection<Commodity>();
+            this.TradingPorts = new ObservableCollection<TradingPort>();
+        }
     }
 }
